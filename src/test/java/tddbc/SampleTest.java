36997731de0c649c0fdf46cdccfc8ff81881cf73
junpob <37261668+junpob@users.noreply.github.com>
2020-08-01 14:26:47 +0900

    test

--- src/test/java/tddbc/SampleTest.java
+++ src/test/java/tddbc/SampleTest.java
@@ -7,14 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SampleTest {
 
     @Test
-    @DisplayName("should return Hello TDD Boot Camp")
-    public void _should_return_Hello_TDD_BootCamp() throws Exception {
-        // Setup
-        Sample sut = new Sample();
-        // Exercise
-        String actual = sut.say();
-        // Verify
-        assertEquals("Hello TDD BootCamp!", actual);
+    public void _整数閉区間は文字列表現を返せる(){
+        fail("テスト失敗");
     }
-
 }
