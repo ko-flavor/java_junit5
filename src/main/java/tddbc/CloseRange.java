88d21d293ce8286357f5ac24adaf81ae3fdaf123
KoheiOkamoto <koko72.flavor@gmail.com>
2020-08-07 23:28:33 +0900

    commit source codes written in tddbc2020.

---
+++ src/main/java/tddbc/CloseRange.java
@@ -0,0 +1,33 @@
+package tddbc;
+
+public class CloseRange {
+
+	private final int lowerEndpoint;
+	private final int upperEndpoint;
+
+	public CloseRange(int lowerEndpoint, int upperEndpoint) {
+		if (lowerEndpoint > upperEndpoint) {
+			throw new IllegalArgumentException();
+		}
+		this.lowerEndpoint = lowerEndpoint;
+		this.upperEndpoint = upperEndpoint;
+	}
+
+	public String display() {
+		return "[" + lowerEndpoint + "," + upperEndpoint + "]";
+	}
+
+	public boolean include(int number) {
+		return this.lowerEndpoint <= number && this.upperEndpoint >= number;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof CloseRange) {
+			CloseRange anotherCloseRange = (CloseRange) obj;
+			return anotherCloseRange.display().equals(this.display());
+		}
+		return false;
+	}
+
+}
--- src/test/java/tddbc/SampleTest.java
+++ src/test/java/tddbc/SampleTest.java
@@ -1,13 +1,62 @@
 package tddbc;
 
-import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.DisplayName;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class SampleTest {
 
-    @Test
-    public void _整数閉区間は文字列表現を返せる(){
-        fail("テスト失敗");
-    }
+	CloseRange closeRange_3_8 = new CloseRange(3, 8);
+
+	@Test
+	@DisplayName("下端点3,上端点 8 の整数閉区間の文字列表記は[3,8]")
+	public void 下端点3上端点8の整数閉区間の文字列表記() {
+		assertThat(closeRange_3_8.display(), is("[3,8]"));
+	}
+
+	@Test
+	public void 下端点8上端点3の整数閉区間は生成できない() {
+		assertThrows(IllegalArgumentException.class, () -> new CloseRange(8, 3));
+	}
+
+	@Test
+	public void 下端点3上端点3の整数閉区間は生成できる() {
+		new CloseRange(3, 3);
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間に8が含まれる() {
+		assertThat(closeRange_3_8.include(8), is(true));
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間に9が含まれない() {
+		assertThat(closeRange_3_8.include(9), is(false));
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間に3が含まれる() {
+		assertThat(closeRange_3_8.include(3), is(true));
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間に2が含まれない() {
+		assertThat(closeRange_3_8.include(2), is(false));
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間と下端点3上端点8の整数閉区間が等価であると判定できる() {
+		CloseRange anotherCloseRange = new CloseRange(3, 8);
+		assertThat(closeRange_3_8.equals(anotherCloseRange), is(true));
+	}
+
+	@Test
+	public void 下端点3上端点8の整数閉区間と下端点2上端点9の整数閉区間が等価であると判定できる() {
+		CloseRange anotherCloseRange = new CloseRange(2, 9);
+		assertThat(closeRange_3_8.equals(anotherCloseRange), is(false));
+	}
+
 }
